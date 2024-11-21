29aabd6bc7da5e63da62d8e5d9d712da5b727e51
wavideo <kt8218@gmail.com>
2024-11-21 18:27:55 +0900

    1. 스윙 코드 정리, 최적화 2. 스윙 <-> 점프 스위칭 모드 개발

--- app/src/main/java/com/studiowavi/hwick/MainActivity.kt
+++ app/src/main/java/com/studiowavi/hwick/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.studiowavi.hwick
 
-import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.media.MediaPlayer
 import android.media.SoundPool
 import android.os.Bundle
 import android.util.Log
 import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.values
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.studiowavi.hwick.databinding.ActivityMainBinding
 import kotlin.math.abs
 import kotlin.math.sqrt
@@ -22,16 +17,17 @@ import kotlin.math.sqrt
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var binding : ActivityMainBinding
 
+    // 효과음 변수 세팅
     private lateinit var soundPool : SoundPool
-    private var soundId : Int = 0
+    private var soundIdSwing : Int = 0
+    private var soundIdJump : Int = 0
+    private var sfxType = "swing"
 
+    // 센서 변수 세팅
     private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
-
-    private val shakeThreshold = 50.0f
-    private val minSoundInterval = 300L // 최소 소리 재생 간격 (500ms)
-    private var lastSoundTime = 0L // 마지막 소리 재생 시간 기록
     private var isSwinging = false // 휘두름 상태 플래그
+    private val swingThreshold = 80.0f // 센서 제어값 상수 세팅
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,12 +36,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         setContentView(binding.root)
 
         initSoundPool()
-        initOptionPicker()
-        initPlayButton()
-
+        initLayout()
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
     }
 
     override fun onResume() {
@@ -60,6 +53,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager.unregisterListener(this)
     }
 
+    // ================================
+    // 운동 센서 파트
+    // ================================
     override fun onSensorChanged(event: SensorEvent?) {
         event?.let {
             if (it.sensor.type == Sensor.TYPE_ACCELEROMETER) {
@@ -67,18 +63,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 val y = it.values[1]
                 val z = it.values[2]
 
-                val accelerationMagnitude = sqrt(x * x + y * y + z * z)
-
-                if (accelerationMagnitude > shakeThreshold) {
-                        // 휘두름 감지 처리
-                        Log.d("센서","감지시작")
+                when (sfxType) {
+                    "swing" -> {
                         handleSwinging(x, y, z)
                     }
-                    else {
-                        Log.d("센서","멈춤")
-                        // 가속도가 줄어들면 휘두름 종료
-                        isSwinging = false
+                    "jump" -> {
+                        handleJumping(x, y, z)
                     }
+                }
             }
         }
     }
@@ -88,39 +80,69 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     //  휘두름 동작 처리
     private fun handleSwinging(x: Float, y: Float, z: Float) {
-        val currentTime = System.currentTimeMillis()
+        val accelerationMagnitude = sqrt(x * x + y * y + z * z)
 
-        if (!isSwinging) {
-            //  새로운 휘두름 동작 시작
-            isSwinging = true
-            lastSoundTime = currentTime
-            playSound()
-        } else {
-            // 같은 방향에서의 휘두름 유지 시 추가 소리 차단
-            if (currentTime - lastSoundTime >= minSoundInterval) {
-                // 특정 축의 변화량으로 방향 유지 확인
-                if (abs(x) > abs(y) && abs(x) > abs(z)) {
-                    // X축을 따라 길게 유지 중
-                    Log.d( "센서","X축 유지 중")
-                } else if (abs(y) > abs(x) && abs(y) > abs(z)) {
-                    // Y축을 따라 길게 유지 중
-                    Log.d( "센서","Y축 유지 중")
-                } else {
-                    // Z축을 따라 길게 유지 중
-                    Log.d( "센서","Z축 유지 중")
-                }
+        if (accelerationMagnitude > swingThreshold) {
+            // 휘두름 감지 처리
+
+            if (!isSwinging) {
+                //  새로운 휘두름 동작 시작
+                isSwinging = true
+                playSound("swing")
             }
+        }
+        else {
+            // 가속도가 줄어들면 휘두름 종료
+            isSwinging = false
         }
     }
 
+    private fun handleJumping(x: Float, y: Float, z: Float) {
+
+    }
+
+    // ================================
+    // 레이아웃 & 사운드 세팅 파트
+    // ================================
+
     fun initSoundPool() {
         soundPool = SoundPool.Builder().setMaxStreams(10).build()
-        soundId = soundPool.load(this, R.raw.sfx_swosh_001, 1)
+        soundIdSwing = soundPool.load(this, R.raw.sfx_swosh_001, 1)
+        soundIdJump = soundPool.load(this, R.raw.sfx_swosh_003, 1)
         soundPool.setOnLoadCompleteListener { soundPool, sampleId, status ->
         }
     }
 
-    fun initOptionPicker() {
+    fun playSound(type:String) {
+        when(type){
+            "swing" -> {
+                soundPool.play(soundIdSwing, 5.0f, 5.0f, 0, 0, 1.0f)
+            }
+            "jump" -> {
+                soundPool.play(soundIdJump, 5.0f, 5.0f, 0, 0, 1.0f)
+            }
+        }
+    }
+
+    private fun initLayout() {
+        initSwitchButton()
+        initOptionPickerSwing()
+    }
+
+    fun initSwitchButton(){
+        binding.btnSwing.setOnClickListener {
+            playSound("swing")
+            sfxType = "swing"
+        }
+
+        binding.btnJump.setOnClickListener {
+            playSound("jump")
+            sfxType = "jump"
+        }
+    }
+
+    fun initOptionPickerSwing() {
+
         binding.btnOptionPicker.setOnClickListener { view ->
             val popupMenu = PopupMenu(this, view)
             popupMenu.inflate(R.menu.popup_menu)
@@ -128,18 +150,18 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             popupMenu.setOnMenuItemClickListener { item ->
                 when (item.itemId) {
                     R.id.option1 -> {
-                        soundId = soundPool.load(this, R.raw.sfx_swosh_001, 1)
-                        binding.btnOptionPicker.text = "옵션1"
+                        soundIdSwing = soundPool.load(this, R.raw.sfx_swosh_001, 1)
+                        binding.btnOptionPicker.text = getString(R.string.sfx_swosh_001)
                         true
                     }
                     R.id.option2 -> {
-                        soundId = soundPool.load(this, R.raw.sfx_swosh_002, 1)
-                        binding.btnOptionPicker.text = "옵션2"
+                        soundIdSwing = soundPool.load(this, R.raw.sfx_swosh_002, 1)
+                        binding.btnOptionPicker.text = getString(R.string.sfx_swosh_002)
                         true
                     }
                     R.id.option3 -> {
-                        soundId = soundPool.load(this, R.raw.sfx_swosh_003, 1)
-                        binding.btnOptionPicker.text = "옵션3"
+                        soundIdSwing = soundPool.load(this, R.raw.sfx_swosh_003, 1)
+                        binding.btnOptionPicker.text = getString(R.string.sfx_swosh_001)
                         true
                     }
                     else -> false
@@ -147,16 +169,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
 
             popupMenu.show()
-        }
-    }
-
-    fun playSound() {
-        soundPool.play(soundId, 5.0f, 5.0f, 0, 0, 1.0f)
-    }
-
-    fun initPlayButton() {
-        binding.btnPlay.setOnClickListener {
-            playSound()
         }
     }
 
